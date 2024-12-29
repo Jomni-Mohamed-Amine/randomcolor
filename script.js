a9e5beffd9e9c8f913d9d859da434e3602841602
Mohamed amin <amin.jomni77@gmail.com>
2024-12-29 20:44:07 +0100

    second commiter

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
-// const red=document.getElementById('red')
-// const blue=document.getElementById('blue')
-// const green=document.getElementById('green')
-// const yellow=document.getElementById('yellow')
+var $btn = document.getElementById('btn');
+var $bg = document.querySelector('.bgcolor');
 
-// function changeBackgroundColor(color) {
-//     document.body.style.backgroundColor = color;
-// }
+function colors(){
+    return Math.floor(Math.random()*255)
+}
+$btn.addEventListener('click',function(){
+var red=colors();
+var blue=colors();
+var yellow=colors();
+$bg.style.backgroundColor=`rgb(${red},${blue},${yellow})`
 
-// red.addEventListener('click', () => changeBackgroundColor('red'));
-// blue.addEventListener('click', () => changeBackgroundColor('blue'));
-
-// green.addEventListener('click', () => changeBackgroundColor('green'));
-
-// yellow.addEventListener('click', () => changeBackgroundColor('yellow'));
-// Get the dropdown button and menu
-// const dropdownBtn = document.getElementById('p-dropdown');
-// const dropdownMenu = document.getElementsByClassName('.list')
-
-// dropdownBtn.addEventListener('click', function() {
-//     if (dropdownMenu.style.visibility== 'hidden') {
-//         dropdownMenu.style.visibility= 'visible'; 
-//         dropdownMenu.style.opacity= 1; 
-
-
-//     } else {
-//         dropdownMenu.style.visibility= 'hidden'; 
-//         dropdownMenu.style.opacity= 0; 
-
-//     }
-// });
-// function 
-
-
-
+})
