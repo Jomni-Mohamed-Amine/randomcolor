4738ba28dc0ddf37f12513f4304f84e88e00d92f
Mohamed amin <amin.jomni77@gmail.com>
2024-12-29 20:36:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// const red=document.getElementById('red')
+// const blue=document.getElementById('blue')
+// const green=document.getElementById('green')
+// const yellow=document.getElementById('yellow')
+
+// function changeBackgroundColor(color) {
+//     document.body.style.backgroundColor = color;
+// }
+
+// red.addEventListener('click', () => changeBackgroundColor('red'));
+// blue.addEventListener('click', () => changeBackgroundColor('blue'));
+
+// green.addEventListener('click', () => changeBackgroundColor('green'));
+
+// yellow.addEventListener('click', () => changeBackgroundColor('yellow'));
+// Get the dropdown button and menu
+// const dropdownBtn = document.getElementById('p-dropdown');
+// const dropdownMenu = document.getElementsByClassName('.list')
+
+// dropdownBtn.addEventListener('click', function() {
+//     if (dropdownMenu.style.visibility== 'hidden') {
+//         dropdownMenu.style.visibility= 'visible'; 
+//         dropdownMenu.style.opacity= 1; 
+
+
+//     } else {
+//         dropdownMenu.style.visibility= 'hidden'; 
+//         dropdownMenu.style.opacity= 0; 
+
+//     }
+// });
+// function 
+
+
+
